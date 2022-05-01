fbc2af48aad69782c602e1844b15a85398b5935f
Cencious <user.email>
2022-05-02 01:35:39 +0300

    add config parent and child classes

--- config.py
+++ config.py
@@ -1,2 +1,21 @@
 import os
 
+class config:
+    '''
+    Configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'http://newsapi.org/v2/news?api_key={}
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
