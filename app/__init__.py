f85e2319e050e42cc4ab8b17404f5584ddd5c69d
Cencious <user.email>
2022-05-02 01:52:49 +0300

    import config settings

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from config import config_options
 
 def create_app(config_name):
     
-    
+     app = Flask(__name__)
+     
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+
 
     
