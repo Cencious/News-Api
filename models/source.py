f5d98fd839d18d5552b6572c32d7d516c9919361
Cencious <user.email>
2022-05-02 23:15:30 +0300

    add class sources to define the news source objects.

---
+++ models/source.py
@@ -0,0 +1,10 @@
+class sources:
+    '''
+    Sources class to define the news source objects
+    '''
+    def __init__(self, id, name, description,url, category)
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
