ea630cfff8d65d91cf1dc3f7fba60e5450ece7d3
Cencious <user.email>
2022-05-02 22:53:42 +0300

    add create_app fuction

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,26 @@
 from flask import Flask
-from config import config_options
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
     
-     app = Flask(__name__)
-
-    # Creating the app configurations
+    app = Flask(__name__)
+    
+    # Creating the app configurations.
     app.config.from_object(config_options[config_name])
-
-    # Initializing flask extensions
+    
+    # Intitializing flask extensions
     bootstrap.init_app(app)
-
+    
     # Registering the blueprint
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    return app 
-
-
-
     
+    app.register_blueprint(main_blueprint)
+    # setting up config
+    from .requests import configure_request
+    
+    configure_request(app)
+    
+    return app
