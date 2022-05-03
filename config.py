8eda06e468cce7110debeb2c28c7bd362e4af0c3
Cencious <user.email>
2022-05-03 16:50:56 +0300

    cange api key on url

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
-class config:
-    '''
-    Configuration parent class
-    '''
-    NEWS_API_BASE_URL = 'http://newsapi.org/v2/news?api_key={}
+class Config:
+    """
+    General configuration parent class
+    """
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=bc65644257bd47a4acce597b88496581'
+    ALL_NEWS_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=bc65644257bd47a4acce597b88496581'
+
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -12,14 +14,9 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development':DevConfig,
+    'production':ProdConfig
 }
