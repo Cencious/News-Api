97daa0b8a0db0c1bb7eba292ca5b37d5ac900e52
Cencious <user.email>
2022-05-03 16:39:37 +0300

    add class source

---
+++ app/models_sources.py
@@ -0,0 +1,13 @@
+class Source:
+    """
+    Defines the various source objects
+    """
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
