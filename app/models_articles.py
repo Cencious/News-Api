ed2e6c238c19fd53cdcfa078ab2cf05e73b9b977
Cencious <user.email>
2022-05-03 16:40:08 +0300

    add class articles

---
+++ app/models_articles.py
@@ -0,0 +1,11 @@
+class Articles:
+    """
+    Defines the article objects
+    """
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
